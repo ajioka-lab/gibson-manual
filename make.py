8b16b1979c9d5fb5ea31e00694eab7f6b6ae957f
Bill Collins <bill@gibthon.org>
2013-05-16 17:59:45 +0100

    Less CSS

--- make.py
+++ make.py
@@ -4,6 +4,13 @@ from jinja2 import FileSystemLoader, Environment
 import codecs 
 import zipfile
 import os
+from weasyprint import HTML, CSS
+
+from subprocess import Popen
+import shlex
+
+os.chdir( '_build' )
+
 
 templateLoader = FileSystemLoader( searchpath="../_template" )
 
@@ -11,9 +18,11 @@ templateEnv = Environment( loader=templateLoader )
 
 filelist = ['index', 'materials', 'workflow', 'print']
 
-os.chdir( '_build' )
 
-zf = zipfile.ZipFile( 'gibson-manual.zip', 'w' )
+Popen( shlex.split('lessc -x --yui-compress ../_template/less/bootstrap.less css/bootstrap.min.css') )
+
+
+zf = zipfile.ZipFile( 'GibsonManual.zip', 'w' )
 
 for f in filelist:
 	template = templateEnv.get_template( f+'.html' )
@@ -27,3 +36,5 @@ zf.write( 'css/bootstrap.min.css' )
 zf.write( 'js/' )
 zf.write( 'js/bootstrap.min.js' )
 zf.write( 'js/jquery-1.9.1.min.js' )
+
+#HTML('print.html').write_pdf('GibsonManual.pdf')
